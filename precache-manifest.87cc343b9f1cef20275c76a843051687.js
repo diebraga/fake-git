6f1c257c64cb4a8280014300d4dddd56c128c87b
unknown <diebraga0055@gmail.com>
2020-05-25 14:42:46 +0100

    Updates

---
+++ precache-manifest.87cc343b9f1cef20275c76a843051687.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "580b3f2e55408b348df4b9c82ce19b47",
+    "url": "/frontend_milestone_project/index.html"
+  },
+  {
+    "revision": "24a3a10dd8762bbb1f12",
+    "url": "/frontend_milestone_project/static/js/2.7f2a5f9e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/frontend_milestone_project/static/js/2.7f2a5f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8447d9f53579eb4d561",
+    "url": "/frontend_milestone_project/static/js/main.9ac0b579.chunk.js"
+  },
+  {
+    "revision": "4b85b28a74b7c40ad658",
+    "url": "/frontend_milestone_project/static/js/runtime-main.b48c21f6.js"
+  }
+]);
